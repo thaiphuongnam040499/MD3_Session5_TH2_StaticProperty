544a298ab89e32ad5c06c20afd08b8eb5c3e7ca1
thaiphuongnam040499 <thainamvinhuni@gmail.com>
2023-03-12 16:40:14 +0700

    th2

---
+++ src/Car.java
@@ -0,0 +1,36 @@
+public class Car {
+    private String name;
+    private String engine;
+    public static int numberOfCars;
+
+    public Car(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public static void setNumberOfCars(int numberOfCars) {
+        Car.numberOfCars = numberOfCars;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public static int getNumberOfCars() {
+        return numberOfCars;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("VinFast", "VF6");
+        System.out.println(Car.numberOfCars);
+        Car car2 = new Car("VinFast", "VF8");
+        System.out.println(Car.numberOfCars);
+    }
+
+}
